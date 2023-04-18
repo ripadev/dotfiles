f3938491f227b3555cb691a7f9cd7bbd736acc87
riparuk <faruqirifa@gmail.com>
2023-04-19 03:05:55 +0700

    safe commit point

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,7 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/	/*Update Interval*/	/*Update Signal*/
-	{"",		"battery",	5,		3 },
+	{"",		"internet",	5,	4},
+	{"",		"volume",	1,	10},
+	{"",		"fanspeed",	5,	12},
+	{"",		"brightness",	1,	11},
+	{"",		"memory",	10,	14},
+	{"",		"cpu",		10,	18},
+	{"",		"battery",	5,	3},
+	{"",		"clock",	20,	1},
+	{"",		"capslock",	1,	2},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
