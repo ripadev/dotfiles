e7be5eaa53647423ae81fd621ec50bad4d989947
riparuk <faruqirifa@gmail.com>
2023-04-20 05:17:32 +0700

    fix small changes

--- symlinkdotfiles.sh
+++ symlinkdotfiles.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 echo "Script Directory : $SCRIPT_DIR"
 echo "Make sure that the file to symlink from deleted!"
 echo "symlink ~/.xinitrc to $SCRIPT_DIR/.xinitrc file..., Please Dont remove this dotfiles folder"
-ln -s "$SCRIPT_DIR/.xinitrc" ~/.xinitrc && echo ".xinitrc symlinked"
+ln -sf "$SCRIPT_DIR/.xinitrc" ~/.xinitrc && echo ".xinitrc symlinked"
 echo "symlink ~/.bashrc to $SCRIPT_DIR/.bashrc file..., Please Dont remove this dotfiles folder"
-ln -s "$SCRIPT_DIR/.bashrc" ~/.bashrc && echo ".bashrc symlinked"
+ln -sf "$SCRIPT_DIR/.bashrc" ~/.bashrc && echo ".bashrc symlinked"
 
-#ln -s "$SCRIPT_DIR/.vimrc" ~/.xinitrc && echo ".vimrc symlinked"
+#ln -sf "$SCRIPT_DIR/.vimrc" ~/.xinitrc && echo ".vimrc symlinked"
