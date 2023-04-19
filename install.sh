677699511a7a007b1aee4903481ee5a3f0a5430b
riparuk <faruqirifa@gmail.com>
2023-04-20 06:33:39 +0700

    add for dmenu

--- install.sh
+++ install.sh
@@ -5,9 +5,10 @@ echo "Run pacmanpkglist.sh, it may to be needed packages"
 source $SCRIPT_DIR/pacmanpkglist.sh
 echo "Run Symlinkdotfiles.sh to symlink to Dotfiles/.* files..."
 source $SCRIPT_DIR/symlinkdotfiles.sh
-echo "make clean install dwm, st, and dwmblocks..."
+echo "make clean install dwm, st, dwmblocks, and dmenu..."
 cd "$SCRIPT_DIR/dwm" && sudo make clean install
 cd "$SCRIPT_DIR/st" && sudo make clean install
 cd "$SCRIPT_DIR/dwmblocks" && sudo make clean install
+cd "$SCRIPT_DIR/dmenu" && sudo make clean install
 mkdir -p ~/.local/bin && cp $SCRIPT_DIR/dwmblocks/bin/* ~/.local/bin/
 
