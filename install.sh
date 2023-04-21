77e0dc049ed1b4db98bf2e67b74747f8a6f07642
riparuk <faruqirifa@gmail.com>
2023-04-21 14:20:24 +0700

    simplify name

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 echo "Run pacmanpkglist.sh, it may to be needed packages"
 source $SCRIPT_DIR/pacmanpkglist.sh
 echo "Run Symlinkdotfiles.sh to symlink to Dotfiles/.* files..."
-source $SCRIPT_DIR/symlinkdotfiles.sh
+source $SCRIPT_DIR/symlink
 echo "make clean install dwm, st, dwmblocks, and dmenu..."
 cd "$SCRIPT_DIR/dwm" && sudo make clean install
 cd "$SCRIPT_DIR/st" && sudo make clean install
