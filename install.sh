e66a2f2f9b8e3c6a74d62fc3fc507612352c27c6
riparuk <faruqirifa@gmail.com>
2023-04-27 06:11:45 +0700

    make it simple

--- install.sh
+++ install.sh
@@ -7,15 +7,11 @@ read -p "Install dwm, dmenu, st, dwmbloks and needed packages? (yes/no) " yn
 case $yn in 
 	yes ) echo ok, we will proceed;
 		sudo pacman --needed -S - < $SCRIPT_DIR/pkglist.txt;
-		git clone https://github.com/riparuk/dwm $SCRIPT_DIR/dwm;
-		git clone https://github.com/riparuk/dmenu $SCRIPT_DIR/dmenu;
-		git clone https://github.com/riparuk/st $SCRIPT_DIR/st;
-		git clone https://github.com/riparuk/dwmblocks $SCRIPT_DIR/dwmblocks;
-		cd "$SCRIPT_DIR/dwm" && sudo make clean install;
-		cd "$SCRIPT_DIR/st" && sudo make clean install;
-		cd "$SCRIPT_DIR/dwmblocks" && sudo make clean install;
-		cd "$SCRIPT_DIR/dmenu" && sudo make clean install;
-		mkdir -pv ~/.local/bin && cp -rfv $SCRIPT_DIR/dwmblocks/bin/* ~/.local/bin/ && cp -rfv $SCRIPT_DIR/startdwm ~/.local/bin;;
+		echo "Cloning and installing needed repository.....";
+		git clone https://github.com/riparuk/dwm $SCRIPT_DIR/dwm && cd "$SCRIPT_DIR/dwm" && sudo make clean install && echo "dwm.....INSTALLED!";
+		git clone https://github.com/riparuk/dmenu $SCRIPT_DIR/dmenu &&	cd "$SCRIPT_DIR/dmenu" && sudo make clean install && echo "dmenu.....INSTALLED!";
+		git clone https://github.com/riparuk/st $SCRIPT_DIR/st && cd "$SCRIPT_DIR/st" && sudo make clean install && echo "st.....INSTALLED!";
+		git clone https://github.com/riparuk/dwmblocks $SCRIPT_DIR/dwmblocks &&	cd "$SCRIPT_DIR/dwmblocks" && sudo make clean install && mkdir -pv ~/.local/bin && cp -rfv $SCRIPT_DIR/dwmblocks/bin/* ~/.local/bin/ && cp -rfv $SCRIPT_DIR/startdwm ~/.local/bin && echo "dwmblocks.....INSTALLED!";;
 	no ) echo Skipping...;;
 	* ) echo invalid response;
 		exit 1;;
@@ -31,4 +27,4 @@ case $yn in
 		exit 1;;
 esac
 
-
+echo "===Please reboot to take effect!==="
